f797ec756099c83510414702742feec4c4debd18
Ahmed Gamal <107142017+ahmedgamaw@users.noreply.github.com>
2022-06-08 21:50:03 +0200

    Add files via upload

---
+++ Shaghalny.js
@@ -0,0 +1,33 @@
+function login() {
+  if (document.getElementById('User').checked == true ) {
+   window.location.href = "Profile.html";
+   }
+  else{
+   window.location.href = "CompanyProfile.html"
+   }
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides"); 
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
